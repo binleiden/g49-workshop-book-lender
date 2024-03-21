d38c4ad6233c828e31470ca75ebd5bdd93864929
binleiden <daniel.petersson83@gmail.com>
2024-03-21 10:43:21 +0100

    Constructor, Setter, Getter.

--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -3,16 +3,58 @@ package se.lexicon.model;
 public class Person {
     // todo: needs completion
     // Fields
-    private int sequencer;
-    private int id;
+    private static int sequencer = 0;
+    private final int id;
     private String firstName;
     private String lastName;
 
     //Constructors
 
+    public Person(String firstName, String lastName) {
+        this.id= getNextId();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    private static int getNextId(){
+        return ++sequencer;
+    }
+
+
     //Setters
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     //Getters
 
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
     //Methods
+    public String getPersonInformation(){
+        return "ID: " + id + "\n" +
+                "Name: " + firstName +" " + lastName;
+
+    }
+    public void loanBook(){
+        //todo: method
+    }
+
+    public void returnBook(){
+        //todo: method
+    }
 }
