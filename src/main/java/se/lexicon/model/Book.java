2c132328c0d5d6bc4e6bb100f8416c86ed657830
binleiden <daniel.petersson83@gmail.com>
2024-03-21 09:37:54 +0100

    Fields created.

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -2,4 +2,16 @@ package se.lexicon.model;
 
 public class Book {
     // todo: needs completion
+    // Fields
+    private String id;
+    private String title;
+    private String author;
+    private Boolean available;
+    //Constructors
+
+    //Setters
+
+    //Getters
+
+    //Methods
 }
--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -2,4 +2,17 @@ package se.lexicon.model;
 
 public class Person {
     // todo: needs completion
+    // Fields
+    private int sequencer;
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    //Constructors
+
+    //Setters
+
+    //Getters
+
+    //Methods
 }
